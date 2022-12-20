ff726ae8c8f95f8234c3a5d11d130f74ddd48c79
Paul <paul@yourcreative.com.au>
2022-12-20 14:21:11 +1100

    redirect

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/index.html',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
