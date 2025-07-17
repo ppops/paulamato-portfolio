b1464910c3f9676f127cabff009d1559628bb7cb
Paul Amato <paul@yourcreative.com.au>
2025-07-17 20:32:55 +1000

    change to next/link

--- components/social/social.tsx
+++ components/social/social.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import {LinkedIn, GitHub, Facebook, Instagram, Twitter} from '@images/icons'
 
 import styles from './social.module.scss'
@@ -6,29 +8,29 @@ export default function Social() {
     return (
         <ul className={styles.Social}>
             <li>
-                <a href="https://www.linkedin.com/in/paul-amato/" target="_blank" rel="noopener noreferrer">
+                <Link href="https://www.linkedin.com/in/paul-amato/" target="_blank" rel="noopener noreferrer">
                     {LinkedIn}
-                </a>
+                </Link>
             </li>
             <li>
-                <a href="https://github.com/ppops" target="_blank" rel="noopener noreferrer">
+                <Link href="https://github.com/ppops" target="_blank" rel="noopener noreferrer">
                     {GitHub}
-                </a>
+                </Link>
             </li>
             <li>
-                <a href="https://facebook.com/amato" target="_blank" rel="noopener noreferrer">
+                <Link href="https://facebook.com/amato" target="_blank" rel="noopener noreferrer">
                     {Facebook}
-                </a>
+                </Link>
             </li>
             <li>
-                <a href="https://www.instagram.com/ppops/" target="_blank" rel="noopener noreferrer">
+                <Link href="https://www.instagram.com/ppops/" target="_blank" rel="noopener noreferrer">
                     {Instagram}
-                </a>
+                </Link>
             </li>
             <li>
-                <a href="https://twitter.com/ppops" target="_blank" rel="noopener noreferrer">
+                <Link href="https://twitter.com/ppops" target="_blank" rel="noopener noreferrer">
                     {Twitter}
-                </a>
+                </Link>
             </li>
         </ul>
     )
